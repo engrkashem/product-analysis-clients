af84a054e5b8f310e9fd7b49a14433f2365b14d7
engrkashem <kashemaust@gmail.com>
2022-04-04 11:03:55 +0600

    Home page responsive done, others responsiveness previously corrected

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,11 +14,11 @@ const Home = () => {
 
     return (
         <div>
-            <div className='flex justify-around items-center mt-16 h-full'>
-                <div className='w-1/2'>
-                    <h1 className='text-6xl font-semibold text-indigo-900'>ONE SHOT..</h1>
+            <div className='sm:flex justify-around items-center mt-16 h-full '>
+                <div className='sm:w-1/2 px-8 '>
+                    <h1 className='text-6xl font-semibold text-indigo-900 text-justify'>ONE SHOT..</h1>
                     <h1 className='text-6xl font-semibold text-indigo-800'>ONE MEMORY !!</h1>
-                    <p className='text-xl mt-5 text-emerald-600'>Photoes are not just Photographs. What fascinates me is not photographs that have an obvious catastrophic history or story behind them, that would make them memorable because of the content</p>
+                    <p className='text-xl mt-5 text-emerald-600 text-justify'>Photoes are not just Photographs. What fascinates me is not photographs that have an obvious catastrophic history or story behind them, that would make them memorable because of the content</p>
                     <button onClick={() => navigate('/about')} className='py-5 px-10 bg-teal-400 text-indigo-800 font-bold mt-10 rounded-lg text-lg'>Packages</button>
                 </div>
                 <div>
