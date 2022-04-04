b802a62f9a4970025f975c72bfd06f1ef825db09
engrkashem <kashemaust@gmail.com>
2022-04-04 13:03:51 +0600

    Analytic chatrts updated. total 5 chart, noe 3 charts added

--- src/components/Analysis/Analysis.js
+++ src/components/Analysis/Analysis.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import useChart from '../../hooks/useChart';
+import InvestmentVsRevenue from '../InvestmentVsRevenue/InvestmentVsRevenue';
 import MonthVsSell from '../MonthVsSell/MonthVsSell';
 
 const Analysis = () => {
@@ -10,14 +11,20 @@ const Analysis = () => {
     return (
         <div>
             <div className='mt-20 '>
-                <h2 className=' text-center my-5 font-bold text-xl text-indigo-900'>Month Vs Sell</h2>
+                <h2 className=' text-center my-5 font-bold text-4xl text-indigo-900'>Month Vs Sell</h2>
                 <MonthVsSell
                     key={'1'}
                     data={data}
                 >
                 </MonthVsSell>)
             </div>
-
+            <div className='mt-20 '>
+                <h2 className=' text-center my-5 font-bold text-4xl text-indigo-900'>Investment Vs Revenue</h2>
+                <InvestmentVsRevenue
+                    key={'2'}
+                    data={data}
+                />
+            </div>
         </div>
     );
 };
---
+++ src/components/InvestmentVsRevenue/InvestmentVsRevenue.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Bar, BarChart, CartesianGrid, Cell, Legend, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
+
+const InvestmentVsRevenue = ({ data }) => {
+
+    const colors = ['#0088FE', '#0000cd', '#FF8042', '#dc143c', '#9932cc', '#FFBB28'];
+
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+        return (
+            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+                {`${(percent * 100).toFixed(0)}%`}
+            </text>
+        );
+    };
+
+    return (
+        <div className='grid grid-cols-1 md:grid-cols-3 justify-between'>
+            <BarChart
+                width={350}
+                height={400}
+                data={data}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="month" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="investment" stackId="a" fill="#8884d8" />
+                <Bar dataKey="revenue" stackId="a" fill="#FF8042" />
+            </BarChart>
+
+            <div>
+                <h3 className='text-center mt-5  font-bold text-xl text-indigo-900'>Investment</h3>
+                <PieChart width={350} height={350}>
+                    <Tooltip />
+                    <Pie
+                        data={data}
+                        cx="50%"
+                        cy="50%"
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                        outerRadius={80}
+                        fill="#8884d8"
+                        dataKey="investment"
+                    >
+                        {data.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+                        ))}
+                    </Pie>
+                </PieChart>
+            </div>
+            <div>
+                <h3 className='text-center mt-5 font-bold text-xl text-indigo-900'>Revenue</h3>
+                <PieChart width={350} height={350}>
+                    <Tooltip />
+                    <Pie
+                        data={data}
+                        cx="50%"
+                        cy="50%"
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                        outerRadius={80}
+                        fill="#8884d8"
+                        dataKey="revenue"
+                    >
+                        {data.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+                        ))}
+                    </Pie>
+                </PieChart>
+            </div>
+
+        </div>
+    );
+};
+
+export default InvestmentVsRevenue;
